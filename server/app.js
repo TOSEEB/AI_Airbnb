1c96fcd01c5c810c7e9542fbde062a92e30241e8
TOSEEB <toseebbeg02@gmail.com>
2026-08-04 16:58:43 +0530

    vercel deployment fix

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const mongoose = require("mongoose");
 
 // const errorHandler = require("./middleware/errorHandler");
 
-// const app = express();
+const app = express();
 
 // // =======================
 // // CORS
