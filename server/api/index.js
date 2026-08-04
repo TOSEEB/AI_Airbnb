2fb31538d38df249df5a1d0a04afbd99718c0a01
TOSEEB <toseebbeg02@gmail.com>
2026-08-04 18:08:02 +0530

    resolving vercel issue

--- server/api/index.js
+++ server/api/index.js
@@ -1,26 +1,17 @@
 console.log("VERCEL ENTRY START");
 
-
 const app = require("../app");
 
 const { connectDatabase } = require("../config/db");
 
 
-// Connect MongoDB
-
-connectDatabase()
-  .then(() => {
-    console.log("DATABASE CONNECTED");
-  })
-  .catch((error) => {
-
-    console.log(
-      "DATABASE CONNECTION ERROR:",
-      error.message
-    );
-
-  });
+let dbPromise = connectDatabase();
 
 
+module.exports = async (req, res) => {
 
-module.exports = app; 
+  await dbPromise;
+
+  return app(req, res);
+
+};
